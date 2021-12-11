c07ca720d628a2b16892e35753756c796149a216
Karsten Borgwaldt <kb@spambri.de>
2021-12-11 13:22:40 +0100

    extract metrics from events instead of counting

--- stuffstream/src/counts.rs
+++ stuffstream/src/counts.rs
@@ -36,7 +36,7 @@ pub(crate) async fn handler(
         .unwrap())
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Request {
     #[serde(deserialize_with = "rfc3339")]
     start: OffsetDateTime,
@@ -45,16 +45,9 @@ pub struct Request {
     query: Option<String>,
     split_by: Option<String>,
     max_buckets: Option<i64>,
-}
-
-impl Request {
-    pub fn has_split_by(&self) -> bool {
-        if let Some(name) = &self.split_by {
-            !name.is_empty()
-        } else {
-            false
-        }
-    }
+    value: Option<String>,
+    aggregate: Option<String>,
+    missing_value_is_zero: Option<bool>,
 }
 
 type Param = (dyn ToSql + Sync);
@@ -74,20 +67,22 @@ fn split_counts_query(
     end_id: usize,
     interval: &CountsInterval,
     max_buckets_id: usize,
+    outer_value_getter: &str,
+    inner_value_getter: &str,
 ) -> String {
     let (getter, split_subquery) = if let Some(split_by) = split_by {
         let getter = format!("coalesce({}, '(null)') as id", split_by);
         let query = format!(
             r#"
-                select {}, count(*) as count
+                select {}, {}
                 from {}
                 where {}
                 and tstamp between ${} and ${}
                 group by 1
-                order by count desc
+                order by subvalue desc
                 limit ${}
             "#,
-            getter, table, expr, start_id, end_id, max_buckets_id
+            getter, inner_value_getter, table, expr, start_id, end_id, max_buckets_id
         );
         (getter, query)
     } else {
@@ -98,13 +93,13 @@ fn split_counts_query(
     format!(
         r#"
             select jsonb_object_agg(tstamp, points) as doc from (
-                select tstamp, jsonb_object_agg(id, count) as points from (
-                    select date_trunc('{}', gen_time) as tstamp, series.id as id, sum(coalesce(subcount, 0)) as count
+                select tstamp, jsonb_object_agg(id, value) as points from (
+                    select date_trunc('{}', gen_time) as tstamp, series.id as id, {}
                     from (select gen_time, id from 
                             generate_series(${}, ${}, '{}'::interval) gen_time,
                             ({}) split
                         ) series
-                    left join (select date_trunc('{}', tstamp) as log_time, {}, count(*) as subcount
+                    left join (select date_trunc('{}', tstamp) as log_time, {}, {}
                             from {}
                             where {}
                             and tstamp between ${} and ${}
@@ -119,12 +114,14 @@ fn split_counts_query(
             ) c
         "#,
         &interval.truncate,
+        outer_value_getter,
         start_id,
         end_id,
         &interval.interval,
         split_subquery,
         &interval.truncate,
         getter,
+        inner_value_getter,
         table,
         expr,
         start_id,
@@ -174,30 +171,63 @@ impl Response {
         Ok((expr, params))
     }
 
+    async fn value_getters(
+        &self,
+        params: Request,
+        param_offset: usize,
+    ) -> Result<(String, String, Vec<Value>), MalformedQuery> {
+        if let Some(value) = params.value {
+            if params.aggregate.is_none() {
+                return Err(MalformedQuery {}); // TODO query is not malformed, parameters don't make sense
+            }
+            let agg = params.aggregate.unwrap();
+
+            let (expr, query_params) = self.parse_identifier(&value, param_offset).await?;
+
+            let coalesce = params.missing_value_is_zero.unwrap_or(false);
+            let outer = if coalesce {
+                format!("{}(coalesce(subvalue, 0)) as value", agg)
+            } else {
+                format!("{}(subvalue) as value", agg)
+            };
+            let inner = format!("{}({}) as subvalue", agg, expr);
+            Ok((outer, inner, query_params))
+        } else {
+            Ok((
+                "sum(coalesce(subvalue, 0)) as value".to_string(),
+                "count(*) as subvalue".to_string(),
+                Vec::new(),
+            ))
+        }
+    }
+
     pub async fn streams(
         self,
         params: Request,
     ) -> impl futures::Stream<Item = Result<impl Into<warp::hyper::body::Bytes>, Error>> {
-        let param_offset = match params.has_split_by() {
-            true => 2,
-            false => 1,
+        let params_clone = params.clone();
+
+        let (expr, mut query_params) = self.parse_query(&params.query, 1).await.unwrap();
+        let getter = if let Some(split_by) = params.split_by {
+            let (getter, getter_params) = self
+                .parse_identifier(&split_by, query_params.len() + 1)
+                .await
+                .unwrap();
+            query_params.extend(getter_params);
+            Some(getter)
+        } else {
+            None
         };
 
-        let (expr, query_params) = self.parse_query(&params.query, param_offset).await.unwrap();
-        let param_offset = param_offset + query_params.len();
+        let (outer_value_getter, inner_value_getter, value_params) = self
+            .value_getters(params_clone, query_params.len() + 1)
+            .await
+            .unwrap();
+        query_params.extend(value_params);
+        let param_offset = query_params.len() + 1;
 
         let db = self.db.get().await.unwrap();
         let interval = CountsInterval::from(params.end - params.start);
-        let (getter, query_params) = if params.has_split_by() {
-            let (getter, mut getter_params) = self
-                .parse_identifier(&params.split_by.unwrap(), 1)
-                .await
-                .unwrap();
-            getter_params.extend(query_params);
-            (Some(getter), getter_params)
-        } else {
-            (None, query_params)
-        };
 
         let query = split_counts_query(
             &self.table,
@@ -207,6 +237,8 @@ impl Response {
             param_offset + 1,
             &interval,
             param_offset + 2,
+            &outer_value_getter,
+            &inner_value_getter,
         );
         let counts = db
             .query_raw(
